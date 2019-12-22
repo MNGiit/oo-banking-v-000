e93032426a0fd4db0123ec4351b9239226be72a6
Mohammed Noori <kmanoorib@gmail.com>
2019-12-22 03:50:43 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -36,7 +36,7 @@ class Transfer
       # @receiver.balance-=@amount
       @receiver.balance = @receiver.balance - @amount
       # @sender.balance+=@amount
-      @sender.balance = @sender.balance - @amount
+      @sender.balance = @sender.balance + @amount
       @status = "reversed"
     end
   end
